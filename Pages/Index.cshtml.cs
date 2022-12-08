613eb926e6747491ed56427208c49c13690f1a78
Javier <jav.ruiz1@gmail.com>
2022-12-08 17:36:14 -0600

    Updates

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -5,15 +5,24 @@ namespace Final_Project_GoW.Pages;
 
 public class IndexModel : PageModel
 {
+  private readonly CharacterGoWContext _context;  
     private readonly ILogger<IndexModel> _logger;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public List<CharacterGoW>CharacterGoW {get; set;} = default!;
+
+    public SelectList CharacterGoWDropDown {get; set;} = default!;
+
+    public IndexModel(CharacterGoWContext context, ILogger<IndexModel> logger)
     {
+        _context = context;
         _logger = logger;
     }
 
     public void OnGet()
     {
+            CharacterGoW = _context.CharacterGoW.ToList();
 
+            CharacterGoWDropDown = new SelectList(CharacterGoW, "CharacterGoWID", "Title");
+            
     }
 }
